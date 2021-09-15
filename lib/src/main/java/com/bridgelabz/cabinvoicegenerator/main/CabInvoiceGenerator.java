1fc3634fda4fbc2e42bd369938e1c6e562cc0430
Prakash Goud <prakash.goud@morningstar.com>
2021-09-16 01:16:43 +0530

    [Prakash] - Added the feature to calcualte fare for multiple rides

--- lib/src/main/java/com/bridgelabz/cabinvoicegenerator/main/CabInvoiceGenerator.java
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/main/CabInvoiceGenerator.java
@@ -1,17 +1,37 @@
 package com.bridgelabz.cabinvoicegenerator.main;
 
+import java.util.ArrayList;
+
 public class CabInvoiceGenerator {
 
 	public static int COST_PER_KM = 10;
 	public static int COST_PER_MIN = 1;
 	public static int MIN_FARE = 5;
+	public double distance;
+	public double time;
+	public double totalFare = 0;
+	
+	public CabInvoiceGenerator(){
+		
+	}
+	
+	public CabInvoiceGenerator(double distance, double time) {
+		this.distance = distance;
+		this.time = time;
+	}
 
-	public double calculateFare(double distance, int time) {
+	public double calculateFare() {
 		double result = (distance * COST_PER_KM) + (time * COST_PER_MIN);
 		if (result < MIN_FARE) {
 			return MIN_FARE;
 		}
 		return result;
+	}
+	public void calFareForMultipleRides(ArrayList<CabInvoiceGenerator> person) {
+		person.stream().forEach(n -> {
+			totalFare = totalFare + n.calculateFare();
+		});
+		System.out.println("The total fare is " + totalFare);
 	}
 
 }
